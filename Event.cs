e52a4fc48d1c66cc94ed541e571bcd9de2e09cf2
NAWSAD ANSARI <nawsad.ansari2016@gmail.com>
2025-10-29 14:34:40 +0530

    Add files via upload

---
+++ Event.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace EventEase.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public DateTime Date { get; set; } = DateTime.Now;
+        public string Description { get; set; } = string.Empty;
+    }
+}
